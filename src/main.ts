ecb57e6e6c357e9595aa73a7f8ba1a8e411f826c
Wessel <wessel@journeyapps.com>
2018-10-09 13:53:08 +0200

    Add sass loader and example scss

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import JourneyIFrameClient from '../node_modules/@journeyapps/journey-iframe-client/dist/bundle';
+import './sass/main.scss';
 
 const journeyIFrameClient = new JourneyIFrameClient();
 
