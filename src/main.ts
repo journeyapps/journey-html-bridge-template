dd0a8da8a2c600f2de434da0570484f50e7768e6
Wessel <wessel@journeyapps.com>
2018-10-09 11:57:24 +0200

    Basic single html inline file

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import JourneyIFrameClient from '../node_modules/@journeyapps/journey-iframe-client/dist/bundle';
+
+const j = new JourneyIFrameClient();
+
+console.log("This is j: ", j);
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+
+module.exports = {
+  entry: './src/main.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'One File',
+      filename: 'index.html',
+      inlineSource: '.(js|css|ts)$'
+    }),
+    new HtmlWebpackInlineSourcePlugin()
+  ],
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: 'ts-loader' }
+    ]
+  }
+};
