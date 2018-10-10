f30ae74923f626d01d34c9a574a6d328ecbee0f7
Wessel <wessel@journeyapps.com>
2018-10-10 13:53:59 +0200

    Add ability to build and upload automatically and use config file.

---
+++ scripts/upload.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const request = require('superagent');
+const yaml = require('yaml');
+
+const config = yaml.parse(fs.readFileSync('config.yml', 'utf8'));
+
+const {token, appId, apiVersion, outputFileName} = config;
+
+function getBasePath() {
+return (
+        'https://build.journeyapps.com/api/' +
+        apiVersion +
+        '/apps/' +
+        appId
+    );
+}
+
+// borrowed shamelessly from milk
+function putRequest(params, content) {
+    let path = getBasePath() + params;
+return request
+    .put(path)
+    .set('content-length', content.byteLength)
+    .set('Authorization', 'Bearer ' + token)
+    .send(content);
+}
+
+const fileData = fs.readFileSync('dist/' + outputFileName);
+
+const uploadRequest = putRequest('/files/mobile/html/' + outputFileName, fileData);
+
+uploadRequest.end(function(err, res) {
+    if (err || !res.ok) {
+        console.log('failure uploading\n', err.response.error);
+    } else {
+        console.log('finished uploading ' + outputFileName);
+    }
+});
+
+
+
