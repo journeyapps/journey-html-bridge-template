97206d2950a6509d7c31684d8f8a5c480a4bb484
Wessel <wessel@journeyapps.com>
2018-10-11 14:49:41 +0200

    Deploy to testing when uploading html file

--- README.md
+++ README.md
@@ -11,9 +11,12 @@ yarn install
 ```
 
 ### Developing
+
+Copy and paste `config_template.yml` and rename it to `config.yml`. This file is gitignored will contain authentication tokens.
+
 The entrypoint of the project is `src/main.ts`. Other files can be imported (required) from this.
 
-An HTML template is in `src/template.html`. It's title can and output filename cang be set in `config.yml`.
+An HTML template is in `src/template.html`. It's title can and output filename can be set in `config.yml`.
 
 ### Building
 To build, run:
--- scripts/upload.js
+++ scripts/upload.js
@@ -18,11 +18,18 @@ return (
 // borrowed shamelessly from milk
 function putRequest(params, content) {
     let path = getBasePath() + params;
-return request
-    .put(path)
-    .set('content-length', content.byteLength)
-    .set('Authorization', 'Bearer ' + token)
-    .send(content);
+    return request
+        .put(path)
+        .set('content-length', content.byteLength)
+        .set('Authorization', 'Bearer ' + token)
+        .send(content);
+}
+
+function postRequest(params, content) {
+    return request
+        .post(getBasePath() + params)
+        .set('Authorization', 'Bearer ' + token)
+        .send(content);
 }
 
 const fileData = fs.readFileSync('dist/' + outputFileName);
@@ -34,8 +41,17 @@ uploadRequest.end(function(err, res) {
         console.log('failure uploading\n', err.response.error);
     } else {
         console.log('finished uploading ' + outputFileName);
+        deploy('testing');
     }
 });
 
-
-
+function deploy(environment) {
+    const deployRequest =  postRequest(`/deploy/${environment}`,'');
+    deployRequest.end(function(err, res) {
+        if (err || !res.ok) {
+            console.log('failure deploying\n', err.response.error);
+        } else {
+            console.log('finished deploying to ' + environment);
+        }
+    });
+}
