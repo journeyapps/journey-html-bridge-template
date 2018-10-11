ab558411d4c341c9e2dc8c56020a2df4ec5320e2
Wessel <wessel@journeyapps.com>
2018-10-11 14:14:11 +0200

    Use public journey-iframe-client

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
-const JourneyIFrameClient = require('@journeyapps/journey-iframe-client');
-// import JourneyIFrameClient from '../node_modules/@journeyapps/journey-iframe-client';
+const JourneyIFrameClient = require('journey-iframe-client');
 import './sass/main.scss';
 
 const journeyIFrameClient = new JourneyIFrameClient();
